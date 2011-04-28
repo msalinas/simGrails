b99ae96bac085d4dbdbb3470797e57ee6cfc54e4
Miguel Toshiba <mikerugerio@gmail.com>
2011-04-28 15:31:49 -0500

    Implementacion personalizada del Dominio Usuario
    Signed-off-by: Miguel Toshiba <mikerugerio@gmail.com>

--- grails-app/controllers/com/sim/usuario/UserController.groovy
+++ grails-app/controllers/com/sim/usuario/UserController.groovy
@@ -1,6 +1,6 @@
 package com.sim.usuario
 
-class UserController {
+class UserController extends grails.plugins.springsecurity.ui.UserController {
 
 	static navigation = [
 		group: 'tabs', action: 'search',title: 'Usuario', order: 40,
