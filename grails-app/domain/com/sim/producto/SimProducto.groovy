a054e5000b8c387425693ac1a6e1e977e2918717
Miguel Rugerio <mrugerio@gmail.com>
2011-08-25 18:28:27 -0500

    Continuación con la implementacion de Productos
    Signed-off-by: Miguel Rugerio <mrugerio@gmail.com>

--- grails-app/domain/com/sim/producto/SimProducto.groovy
+++ grails-app/domain/com/sim/producto/SimProducto.groovy
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import com.sim.catalogo.SimCatMetodoCalculo
 import com.sim.catalogo.SimCatPeriodicidad
 import com.sim.catalogo.SimCatFormaEntrega
+import com.sim.catalogo.SimCatTipoPersona
+import com.sim.regional.SimSucursal
 
 class SimProducto {
 
@@ -20,9 +22,11 @@ class SimProducto {
 	Boolean 			garantias = false
 	Boolean 			asignarTodasSucursales = true
 	
+	static hasMany = [ participantesCredito : SimCatTipoPersona, sucursales : SimSucursal ]
+	
     static constraints = {
-		claveProducto(size:1..2, unique: true, nullable: false, blank: false)
-		nombreProducto(size:5..20, unique: true, nullable: false, blank: false)
+		claveProducto(size:1..20, unique: true, nullable: false, blank: false)
+		nombreProducto(size:5..50, unique: true, nullable: false, blank: false)
 		fechaInicioActivacion(nullable: false)
 		fechaFinActivacion(nullable: false)
 		aplicaA(inList:["INDIVIDUAL", "GRUPO"] )
