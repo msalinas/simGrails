37bf7ce04b565cdc1f9f89eb1f8e03e7dcd7b80e
Miguel Rugerio <mrugerio@gmail.com>
2011-09-22 11:06:47 -0500

    Refactorización de Entidades por el nombre de Empresa
    Signed-off-by: Miguel Rugerio <mrugerio@gmail.com>

--- grails-app/controllers/com/rs/gral/RsGralMenuController.groovy
+++ grails-app/controllers/com/rs/gral/RsGralMenuController.groovy
@@ -1,22 +1,22 @@
-package com.sim.regional
+package com.rs.gral
 
-class SimRegionalMenuController {
+class RsGralMenuController {
 
     static navigation = [
-        group: 'tabs', action: 'inicioRegional',title: 'Entidades', order: 50,
+        group: 'tabs', action: 'inicioEntidades',title: 'Empresa', order: 5,
 		subItems: [
-			[action:'irRegional', order:0, title:"Regional"],
-			[action:'irSucursal', order:10, title:'Sucursal'],
-			[action:'irTelefonos', order:20, title:'Telefonos'],
-			[action:'irDomicilios', order:30, title:'Domicilios'],
-			[action:'irPersonas', order:40, title:'Personas'],
-			[action:'irEmpleados', order:50, title:'Empleados'],
-			[action:'irClientes', order:60, title:'Clientes'],
-			[action:'irGaranteDespositario', order:70, title:'Garante Depositario']
+			[action:'irPersonas', order:10, title:'Personas'],
+			[action:'irEmpleados', order:20, title:'Empleados'],
+			[action:'irClientes', order:30, title:'Clientes'],
+			[action:'irGaranteDespositario', order:40, title:'Garante Depositario'],
+			[action:'irRegional', order:50, title:"Regional"],
+			[action:'irSucursal', order:60, title:'Sucursal'],
+			[action:'irDomicilios', order:70, title:'Domicilios'],
+			[action:'irTelefonos', order:80, title:'Telefonos']
         ]
 	]
 	
-	def inicioRegional = { }
+	def inicioEntidades = { }
 	
 	def irRegional = {
 		redirect (controller:'simRegional', action:'list')
@@ -42,7 +42,5 @@ class SimRegionalMenuController {
 	def irGaranteDespositario ={
 		redirect (controller:'simClienteGaranteDepositario', action:'list')
 	}
-
-	
 
 }
