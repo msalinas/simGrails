4f92bce101606c6e639b2f19c7e8dc5e0bfd1f57
Miguel Toshiba <mikerugerio@gmail.com>
2011-09-24 07:30:43 -0500

    Se actualizo el plugin Navigator de la version 1.2 a la versión 1.3
    Signed-off-by: Miguel Toshiba <mikerugerio@gmail.com>

--- filesModificados/NavigationTagLib.groovy
+++ filesModificados/NavigationTagLib.groovy
@@ -4,8 +4,6 @@ import org.codehaus.groovy.grails.commons.ServiceArtefactHandler
 class NavigationTagLib {
     static namespace = "nav"
     
-    static REQ_ATTRIB_RESOURCES_LOADED = "navigationplugin.resources.loaded"
-
     def navLabelPrefix = 'navigation.'
     
     def navigationService
@@ -19,7 +17,6 @@ class NavigationTagLib {
         if ((attrs.override != 'true') && !Boolean.valueOf(attrs.override)) {
             out << "<link rel=\"stylesheet\" type=\"text/css\" href=\"${resource(plugin:'navigation', dir:'css', file:'navigation.css')}\"/>"
         }
-        request[REQ_ATTRIB_RESOURCES_LOADED] = true
     }
 
     
@@ -57,12 +54,24 @@ class NavigationTagLib {
         }
     }
     
+    private findItemInGroupByPath(String group, path) {
+        def items = navigationService.byGroup[group]
+        items.find { rootItem ->
+            if (rootItem.path == path) {
+                return rootItem
+            } else {
+                return rootItem?.subItems.find { subItem ->
+                    subItem.path == path
+                }
+            }
+        }
+    }
+    
+    
     /**
      * Iterate over nav sub items in a given category's item (by path or title)
      */
     def eachSubItem = { attrs, body ->
-
-		if(!controllerName) return out
 
         def categ = attrs.group ?: '*'
         def itemTitle = attrs.remove('title')
@@ -78,34 +87,47 @@ class NavigationTagLib {
             }
             searchKey = path
             searchType = 'path'
-        } else if (itemTitle) {
-            searchKey = itemTitle
-            searchType = 'title'
         } else {
-            // No, do we have a controller specified?
-            if (!con) {
-                // No, let's default to reverse mapped active path
-                searchKey = navigationService.reverseMapActivePathFor(controllerName, actionName, params)
-                searchType = "path"
-                searchKeyAuto = true
+            // current implied
+            path = navigationService.reverseMapActivePathFor(controllerName, actionName, params)
+            
+            if (itemTitle) {
+                searchKey = itemTitle
+                searchType = 'title'
             } else {
-                searchKey = con
-                searchType = 'controller'
+                // No, do we have a controller specified?
+                if (!con) {
+                    // No, let's default to reverse mapped active path
+                    searchKey = navigationService.reverseMapActivePathFor(controllerName, actionName, params)
+                    searchType = "path"
+                    searchKeyAuto = true
+                } else {
+                    searchKey = con
+                    searchType = 'controller'
+                }
             }
         }
         
+        // @todo The problem is that its searching on path for the base item
+        // but it needs to locate it based on current controller + action if no path created
+        println "EACH SI: sk $searchKey st $searchType ska $searchKeyAuto"
+
         def var = attrs.var
         
-        def items = navigationService.byGroup[categ]
-
-        // Match by path, title or controller as appropriate
-        def item = items.find { 
-            if (searchType == 'path') {
-                return isItemPathActive(it.path, searchKey)
-            } else {
+        // find the top level item that matches
+        // Match by path, title or controller as appropriated
+        def item
+        if (searchType == 'path') {
+            item = findItemInGroupByPath(categ, searchKey)
+        } else {
+            // top-level items in this group
+            def items = navigationService.byGroup[categ]
+            item = items.find { 
                 return it[searchType] == searchKey
             }
         } 
+        
+        println "EACH SI: root item: ${item}"
 
         // If the user did not specify any way to find the item, we don't spew if there isn't an item
         if (!item && !searchKeyAuto) {
@@ -118,11 +140,33 @@ class NavigationTagLib {
         }
     }
     
-    protected isItemPathActive(itemPath, activePath) {
-        //def maxElems = Math.min(itemPath.size(),activePath.size())
-		def maxElems = Math.min(itemPath.size(),activePath?.size()?:0)
+    protected doPathsIntersect(itemPath, activePath) {
+        println "Checking path intersection: $itemPath vs $activePath"
+        def maxElems = Math.min( itemPath?.size() ?: 0, activePath?.size() ?: 0)
         if (maxElems) {
-            return itemPath[0..maxElems-1] == activePath[0..maxElems-1]
+            return itemPath[0] == activePath[0]
+        } else {
+            return false
+        }
+    }
+        
+    protected isPathFullyEncapsulatedBy(itemPath, activePath) {
+        println "Checking encaps path: $itemPath vs $activePath"
+        def itemSize = itemPath?.size() ?: 0
+        def activeSize = activePath?.size() ?: 0
+        if (itemSize <= activeSize) {
+            return itemPath[0..itemSize-1] == activePath[0..itemSize-1]
+        } else {
+            return false
+        }
+    }
+        
+    protected pathIsActive(itemPath, currentPath) {
+        println "Checking active path: $itemPath vs $currentPath"
+        def itemSize = itemPath?.size() ?: 0
+        def activeSize = currentPath?.size() ?: 0
+        if (itemSize && activeSize && (itemSize <= activeSize)) {
+            return itemPath[0..itemSize-1] == currentPath[0..itemSize-1]
         } else {
             return false
         }
@@ -130,18 +174,21 @@ class NavigationTagLib {
         
     protected iterateItems = { items, var, body, linkParams, activePath ->
         def last = items.size()-1
+        def delegate = new VisibilityDelegate(grailsApplication, pageScope, [
+            session:session,
+            request:request,
+            controllerName:controllerName,
+            actionName:actionName,
+            flash:flash,
+            params:params
+        ])
+        
         items.eachWithIndex { item, i ->
             // isVisible is closure or null/false
             def isVisible = item['isVisible']
             
             if (isVisible instanceof Closure) {
                 isVisible = isVisible.clone()
-                def delegate = new VisibilityDelegate(grailsApplication, [
-                    session:session,
-                    request:request,
-                    flash:flash,
-                    params:params
-                ])
                 isVisible.delegate = delegate
                 isVisible.resolveStrategy = Closure.DELEGATE_FIRST
             }
@@ -149,7 +196,13 @@ class NavigationTagLib {
             if ((isVisible == null) || (isVisible == true ) || isVisible.call()) { 
                 def data = [:]
                 data.putAll(item)
-                
+                def id = data['id']
+                if(id instanceof Closure) {
+                    id = id.clone();
+                    id.delegate = delegate
+                    id.resolveStrategy = Closure.DELEGATE_FIRST
+                    data.id = id.call()
+                }
                 // Create a new map of params for createLink composed of the params from the item plus
                 // (overriden by) those passed in in the linkParams from the tag attribute
                 // ...without ever creating a map unless we need it
@@ -170,18 +223,14 @@ class NavigationTagLib {
                     data.last = true
                 }
                 
-                data.active = isItemPathActive(item.path, activePath)
+                data.active = pathIsActive(item.path, activePath)
+                println "Item: ${item.path} is active ${data.active} because of ${activePath}"
                 out << body(var ? [(var):data] : data)
             }
         }
     }
 
     def renderSubItems = { attrs ->
-        if (!request[REQ_ATTRIB_RESOURCES_LOADED]) 
-            throwTagError("You must invoke the nav:resources tag in the HEAD section of your page before you can use other [nav:*] tags")
-
-		if(!controllerName) return out
-
         def grp = attrs.group ?: '*'
         def id = attrs.id == null ? "subnavigation_${grp == '*' ? 'all' : grp}" : attrs.id
 
@@ -190,7 +239,7 @@ class NavigationTagLib {
             // Resolve parent by controller instead
             //attrs.controller = GrailsClassUtils.getLogicalName(controllerName, 'Controller')
             //attrs.remove('title')
-            sectionCode = controllerName.toLowerCase()
+            sectionCode = controllerName?.toLowerCase() ?: '_unknown'
         } else {
             sectionCode = attrs.title.toLowerCase()
         }
@@ -200,12 +249,15 @@ class NavigationTagLib {
             o << " id=\"${id.encodeAsHTML()}\""
         }
         o << '>'
+        
+        println "DOING EACH SI: ${attrs}"
         o << eachSubItem(attrs, { item ->
             def title = item.title?.toLowerCase()
             def cls = "${item.active ? 'subnavigation_active ' : ''}${item.first ? 'subnavigation_first ' : ''}${item.last ? 'subnavigation_last' : ''}"
             o << "<li"
             if (cls) o << " class=\"${cls.trim()}\""
             o << "><a href=\"${item.link.encodeAsHTML()}\">${message(code:'subnavigation.'+grp+'.'+sectionCode+'.'+title, default:item.title, encodeAs:'HTML')}</a></li><br><br>"
+			//SE INCLUYERON SALTOS DE LINEA PARA MOSTRAR LOS SUBITEMS EN ORDEN VERTICAL
         })
         o << "</ul>"
     }
@@ -214,9 +266,6 @@ class NavigationTagLib {
      * Render nav items in a given category
      */
     def render = { attrs ->
-        if (!request[REQ_ATTRIB_RESOURCES_LOADED]) 
-            throwTagError("You must invoke the nav:resources tag in the HEAD section of your page before you can use other [nav:*] tags")
-
         def grp = attrs.group ?: '*'
         def subitems = attrs.subitems ? Boolean.valueOf(attrs.subitems?.toString()) : false
         def id = attrs.id == null ? "navigation_${grp == '*' ? 'all' : grp}" : attrs.id
@@ -235,7 +284,7 @@ class NavigationTagLib {
             def msgCode = navLabelPrefix+grp+'.'+title
             o << "><a href=\"${it.link.encodeAsHTML()}\">${message(code:msgCode, default:it.title, encodeAs:'HTML')}</a>"
             if (subitems) {
-                o << renderSubItems([group:grp, id:'', title:it.title, params:attr.params])
+                o << renderSubItems([group:grp, id:'', title:it.title, params:attrs.params])
             }
             o << "</li>"
         })
@@ -246,10 +295,12 @@ class NavigationTagLib {
 class VisibilityDelegate {
     def props
     def grailsApplication
-    
-    VisibilityDelegate(app, Map concreteProps) {
+    def model
+
+    VisibilityDelegate(app, m, Map concreteProps) {
         props = concreteProps
         grailsApplication = app
+        model = m
     }
     
     /** 
@@ -258,6 +309,8 @@ class VisibilityDelegate {
     def propertyMissing(String name) {
         if (this.@props.containsKey(name)) {
             return this.@props[name]
+        } else if (this.@model.containsKey(name)) {
+            return this.@model[name]
         } else {
             return this.@grailsApplication.mainContext.getBean( name )
         }
